68d34548fa257e83490da0433e66c756b42e3c04
ANURAGTIGGA <a.aront003@gmail.com>
2023-09-10 21:17:27 +0530

    added evolution in pokemon page

--- src/components/pages/Pokemon/Pokemon.jsx
+++ src/components/pages/Pokemon/Pokemon.jsx
@@ -1,22 +1,47 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import "./pokemon.scss";
 
 export default function Pokemon() {
     const {id} = useParams();
+    const [pokeId, setPokeId] = useState(id)
     const [pokemon, setPokemon] = useState(null);
+    const [evolution, setEvolution] = useState([]);
+    const imgUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/";
     const url = "https://pokeapi.co/api/v2/pokemon/";
 
     useEffect(()=>{
         async function fetchPokemonData() {
-            const res = await axios.get(`${url + id}`);
+            const res = await axios.get(`${url + pokeId}`);
             console.log("result ", res);
+            const species = await axios.get(res.data.species.url);
+            const evolutionRes = await axios.get(species.data.evolution_chain.url);
+            
+            let evolves_to = evolutionRes.data.chain;
+            let evolution = [];
+            while(evolves_to) {
+                const key = evolves_to.species ? evolves_to.species.url : evolves_to[0].species.url;
+                evolution.push(getImgUrl(key));
+                if(evolves_to.evolves_to.length === 0) break;
+                evolves_to = evolves_to.evolves_to[0];
+            }
+            console.log('evolution', evolution)
             setPokemon(res.data);
+            setEvolution(evolution);
+        }
+
+        function getImgUrl(url) {
+            const temp = url.split('/');
+            const id = temp[temp.length-2]
+            return {
+                url: imgUrl + id + '.svg',
+                id
+            }
         }
 
         fetchPokemonData()
-    },[])
+    },[pokeId])
 
 
     return (
@@ -24,7 +49,7 @@ export default function Pokemon() {
             {
                 pokemon ? (
                     <div className='card grass'>
-                        <img src={pokemon.sprites.other.dream_world.front_default} />
+                        <img className='pokemon-img' src={pokemon.sprites.other.dream_world.front_default} />
                         <div className='content'>
                             <div className='id'>#{pokemon.id}</div>
                             <div className='name'>{pokemon.name}</div>
@@ -56,6 +81,20 @@ export default function Pokemon() {
                                     }
                                     </span>
                                 </div>
+                            </div>
+                            <p>Evolutions</p>
+                            <div className='evolutions'>
+                                    {
+                                        evolution && evolution.map((item)=>{
+                                            return (
+                                                <div key={item.id} className='pokemon'>
+                                                    <Link to={`/pokemon/${item.id}`}>
+                                                        <img onClick={()=>setPokeId(item.id)} src={item.url}></img>
+                                                    </Link>
+                                                </div>
+                                            )
+                                        })
+                                    }
                             </div>
                         </div>    
                     </div>
