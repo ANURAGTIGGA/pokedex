cd9767d2806fcc1b30a6c77060ef8081568e5296
ANURAGTIGGA <a.aront003@gmail.com>
2023-09-15 22:15:42 +0530

    completed pokemon list page :)

--- src/components/pages/PokemonList/PokemonList.jsx
+++ src/components/pages/PokemonList/PokemonList.jsx
@@ -1,9 +1,81 @@
+import { useEffect, useRef, useState, useContext } from "react";
 import { useParams } from "react-router-dom"
+import regions from '../../../helper/regionData';
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Card from "../../common/Card/Card";
+import  PokemonContext from "../../../context/pokemonContext.js";
+import Loader from "../../common/Loader/Loader";
+import './pokemonList.scss';
 
 export default function PokemonList() {
-    const {region} = useParams()
+    const {region} = useParams();
+    const fetchList = useRef(true);
+    const [error, setError] = useState(null);
+    const [pokemons, setPokemons] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const url = "https://pokeapi.co/api/v2/pokemon/";
+    const { setSelectedPokemon } = useContext(PokemonContext);
+
+    useEffect(()=>{
+        async function getPokedex() {
+            const pokedex = regions.filter((item)=>{
+                if(item.name === region.toLowerCase()) return item;
+                else{
+                    setError('404');
+                }
+            });
+            const entries = await axios.get(pokedex[0].pokedex);
+            filterEntries(entries.data.pokemon_entries);
+        }
+        function filterEntries(entries) {
+            const finalEntries = entries.map((entry)=>{
+                const item = entry.pokemon_species.url.split('/');
+                return item[item.length-2]
+            })
+            
+            fetchPokemons(finalEntries);
+        }
+        async function fetchPokemons(entries) {
+            console.log(entries);
+            let promises = [];
+            for(let i=0;i<entries.length;i++){
+                promises.push(axios.get(url+entries[i]));
+            }
+
+            Promise.all(promises).then((results) => {
+                setPokemons(results);
+                setLoading(false);
+            })
+        }
+
+        if(fetchList.current){
+            fetchList.current = false;
+            getPokedex();
+        }
+    },[])
+
+    function onHandleCardClick(pokemon){
+        setSelectedPokemon(pokemon);
+    }
 
     return (
-        <h1>Pokemon List Page : {region} region</h1>
+        <div className='pokemon-list'>
+        <h1>Pokemons from <span className='region-name'>{region}</span> region</h1>
+        <div className='pokemon-container'>
+            {
+                pokemons && pokemons.map((pokemon)=>{
+                    return (
+                        <Link to={`/pokemon/${pokemon.data.id}`} onClick={()=>onHandleCardClick(pokemon)} key={pokemon.data.id} >
+                            <Card pokemon={pokemon}></Card>
+                        </Link>
+                    )
+                })
+            }
+        </div>
+        {
+            loading && <Loader></Loader>
+        }
+        </div>
     )
 }
--- src/helper/regionData.js
+++ src/helper/regionData.js
@@ -1,14 +1,14 @@
 const regions = [
-    {"name":"kanto","url":"https://pokeapi.co/api/v2/region/1/"},
-    {"name":"johto","url":"https://pokeapi.co/api/v2/region/2/"},
-    {"name":"hoenn","url":"https://pokeapi.co/api/v2/region/3/"},
-    {"name":"sinnoh","url":"https://pokeapi.co/api/v2/region/4/"},
-    {"name":"unova","url":"https://pokeapi.co/api/v2/region/5/"},
-    {"name":"kalos","url":"https://pokeapi.co/api/v2/region/6/"},
-    {"name":"alola","url":"https://pokeapi.co/api/v2/region/7/"},
-    {"name":"galar","url":"https://pokeapi.co/api/v2/region/8/"},
-    {"name":"hisui","url":"https://pokeapi.co/api/v2/region/9/"},
-    {"name":"paldea","url":"https://pokeapi.co/api/v2/region/10/"}
+    {"name":"kanto","url":"https://pokeapi.co/api/v2/region/1/","pokedex":"https://pokeapi.co/api/v2/pokedex/2/"},
+    {"name":"johto","url":"https://pokeapi.co/api/v2/region/2/","pokedex":"https://pokeapi.co/api/v2/pokedex/3/"},
+    {"name":"hoenn","url":"https://pokeapi.co/api/v2/region/3/","pokedex":"https://pokeapi.co/api/v2/pokedex/4/"},
+    {"name":"sinnoh","url":"https://pokeapi.co/api/v2/region/4/","pokedex":"https://pokeapi.co/api/v2/pokedex/5/"},
+    {"name":"unova","url":"https://pokeapi.co/api/v2/region/5/","pokedex":"https://pokeapi.co/api/v2/pokedex/8/"},
+    {"name":"kalos","url":"https://pokeapi.co/api/v2/region/6/","pokedex":"https://pokeapi.co/api/v2/pokedex/12/"},
+    {"name":"alola","url":"https://pokeapi.co/api/v2/region/7/","pokedex":"https://pokeapi.co/api/v2/pokedex/16/"},
+    {"name":"galar","url":"https://pokeapi.co/api/v2/region/8/","pokedex":"https://pokeapi.co/api/v2/pokedex/27/"},
+    {"name":"hisui","url":"https://pokeapi.co/api/v2/region/9/","pokedex":"https://pokeapi.co/api/v2/pokedex/30/"},
+    {"name":"paldea","url":"https://pokeapi.co/api/v2/region/10/","pokedex":"https://pokeapi.co/api/v2/pokedex/31/"}
 ]
 
 export default regions;
